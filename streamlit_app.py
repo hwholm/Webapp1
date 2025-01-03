f340827ef69c1a1e196f3fd294f83b98049f018a
hwholm <114098923+hwholm@users.noreply.github.com>
2025-01-03 12:45:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,15 @@
 import json
 import requests
 import streamlit as sLit
-from streamlit_lottie import st_lottie
+
 
 sLit.set_page_config(page_title="Portfolio", page_icon=":tada:", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+
 
 
 # LOAD
-data_anim = load_lottieurl("https://lottie.host/671e9cbb-e466-4556-8a88-61bd0d4e21dc/WH5NXnSzyQ.json")
+
 
 
 with sLit.container():
@@ -32,9 +28,7 @@ with sLit.container():
                      am pursuing my PL-300 Power Bi Microsoft Certification.
                      I have advanced knowledge of Excel, SQL, and Python. """)
 
-    with right_column:
-        st_lottie(data_anim, height=300, key="dataswag")
-
+   
 #CONTACT
 with sLit.container():
     sLit.write("---")
